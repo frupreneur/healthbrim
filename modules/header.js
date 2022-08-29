34f116c710b46156517cbe4647efce228028b8a1
frupreneur <frupreneur@gmail.com>
2022-08-29 23:46:50 +0100

    updated og meta data

--- modules/header.js
+++ modules/header.js
@@ -1,7 +1,5 @@
 import { DATABASE } from './DATABASE.js'
 
-
-
 export function header() {
     const head = document.querySelector('head');
     head.innerHTML += `
@@ -15,6 +13,31 @@ export function header() {
   gtag('config', 'G-WR93F70PG6');
 </script>
    `
+
+   let presentLocation = location.pathname.slice(8).slice(0, location.pathname.slice(8).indexOf('/'));
+   if(location.pathname.includes('index')){
+    presentLocation = 'index.html'
+   }
+
+   for(let xDisease in DATABASE){
+    if(DATABASE[xDisease].route === presentLocation){
+        // Meta Data
+        const metaDescription = document.querySelector('meta[name="description"]')
+        const ogTitle = document.querySelector('meta[property="og:title"]')
+        const ogType = document.querySelector('meta[property="og:type"]')
+        const ogDescription = document.querySelector('meta[property="og:description"]')
+        const ogUrl = document.querySelector('meta[property="og:url"]')
+
+        metaDescription.setAttribute('content', DATABASE[xDisease].description)
+        ogTitle.setAttribute('content', `${DATABASE[xDisease].title} - Check your health Status`)
+        ogType.setAttribute('content', 'article')
+        ogDescription.setAttribute('content', DATABASE[xDisease].description)
+        ogUrl.setAttribute('content',  `${window.location.href}`)
+
+    }
+
+   }
+
 
 
 
